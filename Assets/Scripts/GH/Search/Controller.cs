c9cbdeefcf305ce672c6b5f31dddabd4176951d9
Gihoon <kih0976@gmail.com>
2025-09-18 02:40:58 +0900

    도서 검색 페이지 작업 진행중

--- Assets/Scripts/GH/Search/Controller.cs
+++ Assets/Scripts/GH/Search/Controller.cs
@@ -5,12 +5,102 @@ using UnityEngine;
 
 namespace GH_Search
 {
+    public enum ECanvas
+    {
+        Base,
+        Popup,
+        Home,
+        Result,
+        FindLocation,
+        MaxCnt, // 5
+    }
+
+
     public class Controller : MonoBehaviour
     {
         [field: Header("MVC")]
-        [SerializeField] public Model model { get; private set; }
-        [SerializeField] public View view { get; private set; }
+        [field: SerializeField] public Model model { get; private set; }
+        [field: SerializeField] public View view { get; private set; }
 
 
+        private void Awake()
+        {
+            if (null == model) Debug.LogWarning("model 설정 안됨");
+            if (null == view) Debug.LogWarning("view 설정 안됨");
+        }
+
+        private void Start()
+        {
+            InitUI();
+        }
+
+        private void InitUI()
+        {
+            // 모든 Canvas 비활성화
+            for (int i = 0; i < view.CanvasList.Count; ++i)
+            {
+                view.SetCanvasActive((ECanvas)i, false);
+            }
+
+            // Base 와 Home Canvas만 활성화
+            view.SetCanvasActive(ECanvas.Base, true);
+            view.SetCanvasActive(ECanvas.Home, true);
+
+            InitHome();
+            InitResult();
+        }
+
+        #region Home Page
+
+        private void InitHome()
+        {
+            // 책 검색 버튼 초기 설정
+            view.BtnSearch.onClick.RemoveAllListeners();
+            view.BtnSearch.onClick.AddListener(ClickSeach);
+            // 도서 대출 베스트 버튼 초기 설정
+            view.BtnLoanBest.onClick.RemoveAllListeners();
+            view.BtnLoanBest.onClick.AddListener(ClickLoanBest);
+            // 신간 도서 베스트 버튼 초기 설정
+            view.BtnNewBest.onClick.RemoveAllListeners();
+            view.BtnNewBest.onClick.AddListener(ClickNewBest);
+
+        }
+
+        private void ClickSeach()
+        {
+            view.SetCanvasActive(ECanvas.Home, false);
+            view.SetCanvasActive(ECanvas.Result, true);
+        }
+
+        private void ClickLoanBest()
+        {
+            view.SetCanvasActive(ECanvas.Home, false);
+            view.SetCanvasActive(ECanvas.Result, true);
+        }
+
+        private void ClickNewBest()
+        {
+            view.SetCanvasActive(ECanvas.Home, false);
+            view.SetCanvasActive(ECanvas.Result, true);
+        }
+
+        #endregion
+
+        #region Result Page
+
+        private void InitResult()
+        {
+            view.BtnBack.onClick.RemoveAllListeners();
+            view.BtnBack.onClick.AddListener(ClickBackBtn);
+
+        }
+
+        private void ClickBackBtn()
+        {
+            view.SetCanvasActive(ECanvas.Result, false);
+            view.SetCanvasActive(ECanvas.Home, true);
+        }
+
+        #endregion
     }
 }
--- Assets/Scripts/GH/Search/View.cs
+++ Assets/Scripts/GH/Search/View.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GH_Search
 {
@@ -10,15 +12,77 @@ namespace GH_Search
         [SerializeField] private List<Canvas> canvasList;
         public List<Canvas> CanvasList => canvasList;
 
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        #region Home Page
+
+        [Header("Home")]
+        // 검색 버튼
+        [SerializeField] private Button btnSearch;
+        // getter
+        public Button BtnSearch => btnSearch;
+        // 대출 베스트 10 버튼
+        [SerializeField] private Button btnLoanBest;
+        // getter
+        public Button BtnLoanBest => btnLoanBest;
+        // 시간 베스트 10 버튼
+        [SerializeField] private Button btnNewBest;
+        // getter
+        public Button BtnNewBest => btnNewBest;
+
+        #endregion
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        #region Result Page
+
+        [Header("Result")]
+        // 뒤로가기 버튼
+        [SerializeField] private Button btnBack;
+        // getter
+        public Button BtnBack => btnBack;
+
+        #endregion
 
         private void Awake()
         {
-            
+            if (canvasList.Count <= 0 || canvasList.Count > (int)ECanvas.MaxCnt) Debug.LogWarning("canvasList 설정 문제");
+            // 모든 Canvas의 Panel의 raycastTarget off
+            foreach (Canvas canvas in canvasList)
+            {
+                GameObject panelObj = canvas.GetComponentInChildren<PanelMarker>().gameObject;
+                panelObj.GetComponent<Image>().raycastTarget = false;
+            }
+
+            #region Home
+
+            if (null == btnSearch) Debug.LogWarning("btnSearch 미설정");
+            if (null == btnLoanBest) Debug.LogWarning("btnLoanBest 미설정");
+            if (null == btnNewBest) Debug.LogWarning("btnNewBest 미설정");
+
+            #endregion
+
+            #region Result
+
+            if (null == btnBack) Debug.LogWarning("btnBack 미설정");
+
+            #endregion
+
+
         }
 
         private void Start()
         {
             
+        }
+
+        public void SetCanvasActive(ECanvas canvas, bool flag)
+        {
+            canvasList[(int)canvas].gameObject.SetActive(flag);
         }
 
     }
